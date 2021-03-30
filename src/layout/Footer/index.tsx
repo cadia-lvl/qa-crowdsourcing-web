9c7c38a62c1e6bf812d67a7f917a8b634bd1582b
njall skarphedinsson <njallis@gmail.com>
2021-03-30 11:54:54 +0000

    completed footer style

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Outer } from "./styles";
 
 const Footer = () => {
-	return <Outer>Footer</Outer>;
+	return (
+		<Outer>
+			<span>Heim</span>
+			<span>spurningar@spurningar.is</span>
+			<span>GitHub</span>
+			<span>Skilmálar</span>
+			<span>Útskrá</span>
+			<span>Um okkur</span>
+		</Outer>
+	);
 };
 
 export default Footer;
--- src/layout/Footer/styles.ts
+++ src/layout/Footer/styles.ts
@@ -3,11 +3,16 @@ import styled from "styled-components";
 export const Outer = styled.footer`
 	margin-top: 100px;
 	width: 100%;
-	border: 1px solid red;
 	flex: 1;
-	padding: 20px;
 	box-sizing: border-box;
 	display: flex;
 	align-items: flex-end;
 	justify-content: flex-start;
+	span {
+		cursor: pointer;
+		padding: 20px;
+		:first-child {
+			padding-left: 0;
+		}
+	}
 `;
