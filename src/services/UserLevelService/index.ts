7517432b9bb4e4206b20fb9515d9609b305bd5f0
njall skarphedinsson <njallis@gmail.com>
2021-04-16 13:22:10 +0000

    added user level service

---
+++ src/services/UserLevelService/index.ts
@@ -0,0 +1,25 @@
+import { ICON_LVL_1, ICON_LVL_2, ICON_LVL_3 } from "../../static";
+
+const LEVEL_NAMES = [
+	"Byrjandi",
+	"Nemandi",
+	"Stjörnu nemandi",
+	"Dúx",
+	"Kennari",
+	"Prófessor",
+	"Galdrakall",
+];
+
+const LEVEL_ICONS = [ICON_LVL_1, ICON_LVL_2, ICON_LVL_3];
+
+export const mapLevelToString = (level: number) => {
+	const string = LEVEL_NAMES[level];
+	if (string === undefined) return "Gandalfur";
+	return string;
+};
+
+export const mapLevelToIconURL = (level: number) => {
+	const URL = LEVEL_ICONS[level];
+	if (URL === undefined) return "https://picsum.photos/70/70";
+	return URL;
+};
