6611a8361d0b1c816c1400772257ce0594e945a4
njall skarphedinsson <njallis@gmail.com>
2021-04-19 14:15:24 +0000

    added empty tutorial guide

---
+++ src/components/organisms/Tutorial/TutorialGuide/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Outer = styled.div`
+	height: 70px;
+	width: 70px;
+`;
--- src/components/organisms/Tutorial/index.ts
+++ src/components/organisms/Tutorial/index.ts
@@ -1,1 +1,2 @@
 export * from "./Explain";
+export * from "./TutorialGuide";
