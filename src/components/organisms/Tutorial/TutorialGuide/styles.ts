8460299cac19f8c3237dcd3db71de0707f77a1ab
njall skarphedinsson <njallis@gmail.com>
2021-04-19 14:20:02 +0000

    added tutorial guide to layout

--- src/components/organisms/Tutorial/TutorialGuide/styles.ts
+++ src/components/organisms/Tutorial/TutorialGuide/styles.ts
@@ -4,6 +4,9 @@ import { Colors } from "../../../../styles";
 export const Outer = styled.div`
 	height: 70px;
 	width: 70px;
-	border: 3px solid ${Colors.HIGHLIGHT};
+	border: 5px solid ${Colors.HIGHLIGHT};
 	border-radius: 50%;
+	position: fixed;
+	bottom: 50px;
+	right: 50px;
 `;
