0bae614681878ecf19cf91bb6ec962c70f715973
njall skarphedinsson <njallis@gmail.com>
2021-04-26 13:55:07 +0000

    fixed color of button

--- src/components/atoms/Buttons/PlayButton/styles.ts
+++ src/components/atoms/Buttons/PlayButton/styles.ts
@@ -3,15 +3,16 @@ import { Colors } from "../../../../styles";
 
 const GLOW_BTN_HEIGHT = 45;
 export const Button = styled.button`
-	background: ${Colors.DANGER};
+	background: ${Colors.SUCCESS};
 	border: none;
 	outline: none;
-	box-shadow: 0px 0px 25px rgba(246, 78, 96, 0.65);
-	animation: pulse 2s infinite;
+	box-shadow: 0px 0px 25px rgba(27, 197, 189, 0.65);
+	animation: pulse 4s infinite;
 	height: ${GLOW_BTN_HEIGHT}px;
 	width: 120px;
 	border-radius: ${GLOW_BTN_HEIGHT / 2}px;
 	color: #fff;
+	font-weight: 500;
 	cursor: pointer;
 	width: 120px !important;
 `;
