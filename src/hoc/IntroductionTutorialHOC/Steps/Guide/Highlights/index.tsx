9bba11ea9408e9a92ef6c2e4758b1a4c71aa91e8
njall skarphedinsson <njallis@gmail.com>
2021-05-04 09:39:07 +0000

    added highlight example

--- src/hoc/IntroductionTutorialHOC/Steps/Guide/Highlights/index.tsx
+++ src/hoc/IntroductionTutorialHOC/Steps/Guide/Highlights/index.tsx
@@ -1,5 +1,28 @@
 import React from "react";
-
+import { Explain, TutorialGuide } from "../../../../../components";
+import { Outer, ExplainThese, Example, ExampleInner } from "./styles";
+import * as TUTORIAL from "./tutorialItems";
 export const HighLights = () => {
-	return <div>c</div>;
+	return (
+		<Outer>
+			<ExplainThese>
+				<Example>
+					<Explain items={TUTORIAL.explain1}>
+						<span>Dæmi 1</span>
+					</Explain>
+				</Example>
+				<Example>
+					<Explain items={TUTORIAL.explain2}>
+						<span>Dæmi 2</span>
+					</Explain>
+				</Example>{" "}
+				<Example>
+					<Explain items={TUTORIAL.explain3}>
+						<span>Dæmi 3</span>
+					</Explain>
+				</Example>{" "}
+			</ExplainThese>
+			<TutorialGuide />
+		</Outer>
+	);
 };
---
+++ src/hoc/IntroductionTutorialHOC/Steps/Guide/Highlights/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Outer = styled.div`
+	flex: 1;
+	flex-direction: column;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const ExplainThese = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: 15px;
+	width: 100%;
+`;
+
+export const Example = styled.div`
+	border: 1px solid #bebebe;
+	border-radius: 5px;
+	height: 150px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	> div {
+		width: calc(100% - 15px);
+		margin-left: -8px;
+		height: calc(100% - 15px);
+		margin-top: -8px;
+	}
+`;
+
+export const ExampleInner = styled.div`
+	width: 100%;
+	height: 100%;
+`;
