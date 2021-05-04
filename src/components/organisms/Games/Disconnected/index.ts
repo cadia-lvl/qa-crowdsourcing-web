56c41e0285eda31cdc847fa2a7814a7175de5eff
njall skarphedinsson <njallis@gmail.com>
2021-05-04 13:45:03 +0000

    Cwt

---
+++ src/components/organisms/Games/Disconnected/index.ts
@@ -0,0 +1,9 @@
+/**
+ * In this directory are each game round components
+ * disconnected from the state, the connected components
+ * are also experted from another directory called connected
+ *
+ * This is done to provide an interface that can be used
+ * in scenarios when redux state is not needed, f.x.
+ * in introduction tutorials
+ */
