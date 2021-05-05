8975fcdf5eea049910810757875063b18305a748
njall skarphedinsson <njallis@gmail.com>
2021-05-05 12:34:31 +0000

    added select span to tutorial

--- src/hoc/IntroductionTutorialHOC/Steps/Answers/SelectSpan/index.tsx
+++ src/hoc/IntroductionTutorialHOC/Steps/Answers/SelectSpan/index.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Game } from "../../../../../components";
 
 export const SelectSpan = () => {
+	const [firstWord, setFirstWord] = useState(undefined);
+	const [lastWord, setLastWord] = useState(undefined);
+
+	const handleClearRange = () => {
+		setFirstWord(undefined);
+		setLastWord(undefined);
+	};
+
 	return (
 		<Game.Disconnected.SelectSpan
 			question={"Er schmoopy doop floopy doop?"}
 			paragraph={
 				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 			}
-			onFirstWordChange={(index) => null}
-			onLastWordChange={(index) => null}
-			onClearRange={() => null}
+			onFirstWordChange={
+				(setFirstWord as unknown) as (i: number) => void
+			}
+			onLastWordChange={
+				(setLastWord as unknown) as (i: number) => void
+			}
+			onClearRange={handleClearRange}
 			onArchive={() => null}
 			onSubmitSpan={() => null}
-			firstWord={0}
-			lastWord={0}
+			firstWord={firstWord}
+			lastWord={lastWord}
 		/>
 	);
 };
