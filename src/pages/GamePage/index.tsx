8a013e717785c36a1962c53eb7f3c48f3321d18c
njall skarphedinsson <njallis@gmail.com>
2021-05-05 14:21:06 +0000

    cwt

--- src/pages/GamePage/index.tsx
+++ src/pages/GamePage/index.tsx
@@ -28,26 +28,31 @@ export const GamePage = () => {
 					<FlexLoader size={150} />
 				) : (
 					<React.Fragment>
+						{/* PROGRESS BAR */}
 						<ProgressBarContainer>
 							<Game.Utils.GameProgress showIcon />
 						</ProgressBarContainer>
+						{/* CREATE QUESTION ROUND COMPONENT */}
 						<GameWrapper type={GameTypes.writeQuestion}>
 							<Game.Utils.TaskInfoBox title="Búa til spurningu">
 								<Game.Connected.WriteQuestion />
 							</Game.Utils.TaskInfoBox>
 						</GameWrapper>
+						{/* VERIFY QUESTIONS QUALITY ROUND COMPONENT*/}
 						<GameWrapper
 							type={GameTypes.questionQualityAssurance}
 						>
-							<Game.Utils.TaskInfoBox title="Búa til spurningu">
+							<Game.Utils.TaskInfoBox title="Staðfesta spurningu">
 								<Game.Connected.QuestionReview />
 							</Game.Utils.TaskInfoBox>
 						</GameWrapper>
+						{/* SELECT SPAN IN PARAGRAPH ROUND COMPONENT*/}
 						<GameWrapper type={GameTypes.verifyAnswerLocation}>
 							<Game.Utils.TaskInfoBox title="Staðsetja svar">
 								<Game.Connected.SelectSpan />
 							</Game.Utils.TaskInfoBox>
 						</GameWrapper>
+						{/* REVIEW SPAN IN PARAGRAPH ROUND COMPONENT*/}
 						<GameWrapper type={GameTypes.verifyAnswerSpan}>
 							<Game.Utils.TaskInfoBox title="Yfirfara svar">
 								<Game.Connected.SpanReview />
