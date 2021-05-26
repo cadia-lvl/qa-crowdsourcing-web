3eda95818dca19a9c5a82c6ca421bf2dfab20d39
njall skarphedinsson <njallis@gmail.com>
2021-05-26 13:18:26 +0000

    created Cookie spage

---
+++ src/pages/Cookies/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+export const Terms = () => {
+	return (
+		<div>
+			<h1 className="italic">Vafrakökustefna</h1>
+			<p>Uppfært 25. maí 2021</p>
+			<p>
+				Þessi vefsíða notar vafrakökur (e. cookies) til að bæta
+				upplifun þína á vefsíðunni.
+			</p>
+			<h2 className="italic">Hvað er vafrakaka?</h2>
+			<p>
+				Vafrakökur (e. cookies) eru nokkurs konar fótspor sem eru
+				dulkóðaðir textastrengi sem vefsíða geymir á tölvu
+				notandans í formi textaskráar. Þær gerðir af kökum sem við
+				notum eru „lotukökur“ (e. session cookies) og „viðvarandi
+				kökur“ (e. persistent cookies). Lotukökur er tímabundnar og
+				er sjálfkrafa eytt þegar þú yfirgefur vefsíðuna okkar.
+				Viðvarandi kökur eru áfram í tölvunni þinni þar til þú
+				eyðir þeim. Vefgeymsla vafra (e. web storage) gerir
+				vefsvæðum kleift að geyma gögn í tölvu notanda líkt og
+				vafrakökur nema eru einungis lesanlegar notanda megin og er
+				stærri en vafrakaka (e. cookie). Í vefgeymslu vafrans
+				notandans nýtir vefurinn „staðbundina geymslu“ (e. local
+				storage) og „lotu geymslu“ (e. session storage). Gögn í
+				lotu geymslu vafrans eru tímabundin og er sjálfkrafa eytt
+				þegar þú yfirgefur vefsíðuna. Gögn í staðbundinni geymslu
+				vafrans eru áfram í tölvunni þinni þar til þú eyðir þeim.
+			</p>
+			<p>
+				Án notkunnar á vafrakökum og vefgeymslu vafra gæti upplifun
+				þín á síðunni versnað og vefsíðan ekki virkað eins og til
+				var ætlast.
+			</p>
+			<h2 className="italic">
+				Tilgangur með notkun vafrakaka og vefgeymslu vafra.
+			</h2>
+			<p>
+				Vefsíðan notar vafrakökur til að mæla notkun á ýmsum
+				undirsíðum innan vefsíðunnar, en það hjálpar okkur að meta
+				hvað þarf að bæta, til þess notum við vafrakökur fyrir
+				Google Analyctics. Við notum upplýsingarnar til að greina
+				umferð á vefsíðunni.
+			</p>
+			<p>
+				Vafrakökur gera okkur kleift að bjóða upp á aukin þægindi,
+				svo sem innskráningu sem og að vista upplýsingar um þínar
+				stillingar á vefsíðunni. Þegar notendur m.a. loka
+				skilaboðum frá Leiðarvísinum þá vistum við slíkt í
+				staðbundina geymslu til þess að tryggja að skilaboðin
+				birtist ekki aftur.
+			</p>
+			<p>
+				Við notum ekki vafrakökur til að safna upplýsingum er varða
+				heimsóknir þínar á öðrum síðum, né að safna persónulegum
+				upplýsingum um þig, aðrar en þær sem þú hefur skráð hjá
+				okkur af frjálsum vilja í gegnum samskipti við okkar
+				vefsíðu.
+			</p>
+		</div>
+	);
+};
